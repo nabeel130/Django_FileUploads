919946d162b9f7d6ecd198f028f0282e69f04d1b
Mohammed Nabeel km <nabeelkmvkd07@gmail.com>
2020-10-16 15:18:30 +0530

    Add basic uploader

--- fileuploads/views.py
+++ fileuploads/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
+
+
+from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
 class Home(TemplateView):
     template_name = 'fileuploads/home.html'
 
 def upload(request):
-    return render(request, 'fileuploads/upload.html')
-    
+    if request.method == 'POST':
+        uploaded_file = request.FILES['document']
+        # print(uploaded_file.name)
+        # print(uploaded_file.size)
+        fs = FileSystemStorage()
+        name = fs.save(uploaded_file.name , uploaded_file)
+        url = fs.url(name)
+        context = {
+            'url' : url
+        }
+    return render(request, 'fileuploads/upload.html' , context)
     
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from fileuploads.views import Home
+from fileuploads.views import Home ,upload
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Home.as_view() ,name = 'home'),
+    path('upload/', upload ,name = 'upload'),
    
-]
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
